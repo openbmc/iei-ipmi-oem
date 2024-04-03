422d9406b141bd7d1b0df5581bb18ac8146168f8
George Liu <liuxiwei@ieisystem.com>
2024-04-03 09:30:45 +0800

    Updated iei namespace
    Change-Id: I9e623452279b363ee8421e2bd4d2072c2b44031f
    Signed-off-by: George Liu <liuxiwei@ieisystem.com>

--- src/iei_oem.hpp
+++ src/iei_oem.hpp
@@ -4,15 +4,15 @@
 #include <cstdint>
 #include <string_view>
 
-namespace inspur
+namespace iei
 {
 
-enum ipmi_inspur_net_fns
+enum ipmi_iei_net_fns
 {
-    NETFN_OEM_INSPUR = 0x3c,
+    NETFN_OEM_IEI = 0x3c,
 };
 
-enum inspur_oem_cmds
+enum iei_oem_cmds
 {
     CMD_OEM_ASSET_INFO = 0x01,
 };
@@ -46,4 +46,4 @@ constexpr std::array<std::string_view, 8> bios_devname{
     "BIOS", "ME", "IE", "PCH", "BOARD", "MRC", "CUSTOM_ID", "PCH_STEPPING",
 };
 
-} // namespace inspur
+} // namespace iei
--- test/test_iei_ipmi_oem.cpp
+++ test/test_iei_ipmi_oem.cpp
@@ -1,6 +1,6 @@
 #include "config.h"
 
-#include "inspur_oem.hpp"
+#include "iei_oem.hpp"
 #include "mocked_sdbus.hpp"
 #include "mocked_utils.hpp"
 #include "sdbus_wrapper.hpp"
@@ -30,14 +30,14 @@ void ipmi_register_callback(ipmi_netfn_t, ipmi_cmd_t, ipmi_context_t,
     // Empty
 }
 
-class TestInspurIpmiOem : public ::testing::Test
+class TestIpmiOem : public ::testing::Test
 {
   public:
-    TestInspurIpmiOem() :
+    TestIpmiOem() :
         mockedUtils(
             reinterpret_cast<const utils::MockedUtils&>(utils::getUtils()))
     {}
-    virtual ~TestInspurIpmiOem()
+    virtual ~TestIpmiOem()
     {
         utils::freeUtils();
         clearMockedBus();
@@ -47,19 +47,19 @@ class TestInspurIpmiOem : public ::testing::Test
     const utils::MockedUtils& mockedUtils;
 };
 
-TEST_F(TestInspurIpmiOem, Empty)
+TEST_F(TestIpmiOem, Empty)
 {
     // Empty
 }
 
-TEST_F(TestInspurIpmiOem, parseBIOSInfoEmpty)
+TEST_F(TestIpmiOem, parseBIOSInfoEmpty)
 {
 
     EXPECT_CALL(mockedUtils, setPropertyImpl(_, _, _, _, _, _)).Times(0);
     ipmi::parseBIOSInfo({});
 }
 
-TEST_F(TestInspurIpmiOem, parseBIOSInfoValidBIOSVersion)
+TEST_F(TestIpmiOem, parseBIOSInfoValidBIOSVersion)
 {
 
     std::vector<uint8_t> data{
