8a59756a92004b8ef83cd663b2d783e769c9dfdf
George Liu <liuxiwei@ieisystem.com>
2024-06-03 19:44:28 +0800

    dbus: remove getDBus method
    Since the getSdBus method has been implemented in ipmid and
    iei-ipmi-oem already depends on ipmid, there is no need to implement
    the getDBus method again.
    
    Tested: Built iei-ipmi-oem successfully and it works fine.
    
    Change-Id: Ib384c8683f998a49cdcfde22b0948e1f1bdbd80d
    Signed-off-by: George Liu <liuxiwei@ieisystem.com>

--- test/test_iei_ipmi_oem.cpp
+++ test/test_iei_ipmi_oem.cpp
@@ -1,11 +1,7 @@
 #include "config.h"
 
 #include "iei_oem.hpp"
-#include "mocked_sdbus.hpp"
 #include "mocked_utils.hpp"
-#include "sdbus_wrapper.hpp"
-
-#include <ipmid/api.h>
 
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
@@ -40,10 +36,8 @@ class TestIpmiOem : public ::testing::Test
     virtual ~TestIpmiOem()
     {
         utils::freeUtils();
-        clearMockedBus();
     }
 
-    sdbusplus::bus::bus& mockedBus = getBus();
     const utils::MockedUtils& mockedUtils;
 };
 
