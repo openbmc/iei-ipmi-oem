257f26d03d3c683d09399a824c096e099ce557df
John Wang <wangzhiqiang02@ieisystem.com>
2024-06-04 15:00:27 +0800

    Name cpp files with functions
    Instead of implementing all the features in one cpp file, we organize
    the cpp file according to the specific functionality. Here, we renamed
    iei_oem.cpp to asset_info.cpp, and in fact this file only implements the
    features related to asset information
    
    Change-Id: I29d29dd40fb6eb353e77fbf8eb7370bf0b84ccb6
    Signed-off-by: John Wang <wangzhiqiang02@ieisystem.com>

---
+++ src/types.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#include <ipmid/api-types.hpp>
+
+namespace ipmi
+{
+
+constexpr ipmi::NetFn netFnIei = 0x3C;
+constexpr ipmi::NetFn netFnIntelMe = 0x30;
+
+namespace iei
+{
+
+constexpr ipmi::Cmd cmdSetAssetInfo = 0x01;
+constexpr ipmi::Cmd cmdGetBMCFWInfo = 0x37;
+
+} // namespace iei
+
+namespace intel_me
+{
+
+constexpr ipmi::Cmd cmdGetPsuPinReading = 0xE2;
+
+}
+
+} // namespace ipmi
